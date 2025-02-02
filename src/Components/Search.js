831025441a4e2b6c34c1cfa1f6a4760f6c31d36c
PyAbhisek <139870038+PyAbhisek@users.noreply.github.com>
2025-02-02 19:13:31 +0530

    added localstorage support

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,12 +1,16 @@
 import { useContext,useState } from "react"
 import { AppContext } from "./Context/ContextProvider"
 const Search =()=>{
-    const { userInput, setUserInput } = useContext(AppContext)
+    const {setUserInput } = useContext(AppContext)
     const [input,setInput] = useState('')
+    const handleSubmit = ()=>{
+        setUserInput(input)
+        localStorage.setItem('city',input)
+    }
     return(
         <div>
             <input type="text" placeholder="Enter Your City" onChange={(e)=>setInput(e.target.value)}/>
-            <button type="Submit" onClick={()=>setUserInput(input)}>Check</button>
+            <button type="Submit" onClick={handleSubmit}>Check</button>
         </div>
     )
 }
