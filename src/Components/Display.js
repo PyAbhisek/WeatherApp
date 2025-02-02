e282b0f3e180903971d6aed778db4bdfbf48bd20
PyAbhisek <139870038+PyAbhisek@users.noreply.github.com>
2025-02-02 19:24:05 +0530

    error handling added

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -1,30 +1,33 @@
-import { useContext,useEffect } from "react";
+import { useContext,useEffect, useState } from "react";
 import { AppContext } from "./Context/ContextProvider";
 
 const Display =()=>{
-    const {userInput}  = useContext(AppContext)
+    const {userInput,setShowError}  = useContext(AppContext)
+    const [data,setData] = useState({})
     const ApiKey = '8bcaec0c8cddc8f2d262ba7c606bf9a5'
- 
+    // temperature, humidity, wind speed, and weather conditions (
     const fetchWeather = async()=>{
         try{
             let res = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${userInput}&limit=1&appid=${ApiKey}`)
+            if(!res.ok)
+            {
+                setShowError(true)
+            }
             let info = await res.json()
             let weatherData = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${info[0].lat}&lon=${info[0].lon}&appid=${ApiKey}&units=metric`)
             let data = await weatherData.json()
             console.log(data)
+            setData(data)
         }
         catch(err){
             console.log(err)
+            setShowError(true)
         }
         
     }
-
     useEffect(()=>{
         fetchWeather()
     },[userInput])
-    // const fetchWeather = ()=>{
-
-    // } 
     const getForecast =()=>{
 
     }
